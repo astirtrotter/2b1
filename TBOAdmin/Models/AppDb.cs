345187a933208594559960c17b10d3f2a1d1f994
Pro Dev <prodev9999@gmail.com>
2019-06-06 04:53:01 +0800

    Create AppDb.cs

---
+++ TBOAdmin/Models/AppDb.cs
@@ -0,0 +1,21 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+
+namespace TBOAdmin.Models
+{
+  public class AppDb : IDisposable
+  {
+
+    public MySqlConnection Connection;
+
+    public AppDb(string connectionString)
+    {
+      Connection = new MySqlConnection(connectionString);
+    }
+
+    public void Dispose()
+    {
+      Connection.Close();
+    }
+  }
+}
