f92cc6a36b1d5db03a586355470d3a534bfab303
Pro Dev <prodev9999@gmail.com>
2019-06-06 19:30:33 +0800

    Create AppDbContext.cs

---
+++ TBOAdmin/AppDbContext.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TBOAdmin.Models;
+
+namespace TBOAdmin
+{
+  public class AppDbContext : DbContext
+  {
+    public AppDbContext()
+    {
+    }
+
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    public virtual DbSet<User> Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      if (!optionsBuilder.IsConfigured)
+      {
+        optionsBuilder.UseSqlServer(Startup.ConnectionString);
+      }
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+      base.OnModelCreating(modelBuilder);
+
+      modelBuilder.Entity<User>().Ignore(e => e.FullName);
+
+      //modelBuilder.Entity<Blog>(entity =>
+      //{
+      //  entity.Property(e => e.Url).IsRequired();
+      //});
+
+      //modelBuilder.Entity<Post>(entity =>
+      //{
+      //  entity.HasOne(d => d.Blog)
+      //      .WithMany(p => p.Post)
+      //      .HasForeignKey(d => d.BlogId);
+      //});
+    }
+  }
+}
