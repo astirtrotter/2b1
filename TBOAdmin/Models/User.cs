6ba59b7484b1f4468ba43587059da03dc29e8b1b
Pro Dev <prodev9999@gmail.com>
2019-06-06 19:31:49 +0800

    Create User Model

---
+++ TBOAdmin/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TBOAdmin.Models
+{
+  public class User
+  {
+    [Required]
+    public string Id { get; set; }
+
+    [Required, StringLength(20)]
+    public string FirstName { get; set; }
+
+    [Required, StringLength(20)]
+    public string LastName { get; set; }
+
+    public string FullName { get => $"{FirstName} {LastName}"; }
+
+
+  }
+}
