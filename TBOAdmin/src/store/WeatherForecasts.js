3d0fcec55d927255f6edb3aaa68b1c224d25f078
Pro Dev <prodev9999@gmail.com>
2019-06-12 19:55:07 +0800

    Update WeatherForecasts.js

--- TBOAdmin/src/store/WeatherForecasts.js
+++ TBOAdmin/src/store/WeatherForecasts.js
@@ -11,7 +11,7 @@ export const actionCreators = {
 
     dispatch({ type: requestWeatherForecastsType, startDateIndex });
 
-    const url = `api/SampleData/WeatherForecasts?startDateIndex=${startDateIndex}`;
+    const url = `https://localhost:44328/api/SampleData/WeatherForecasts?startDateIndex=${startDateIndex}`;
     const response = await fetch(url);
     const forecasts = await response.json();
 
