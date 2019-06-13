fb8264ea7fa6c0f821e200a4bf5d143cc4571082
Pro Dev <prodev9999@gmail.com>
2019-06-14 06:03:21 +0800

    Update SeedData.cs

--- TBOBackEnd/Database/SeedData.cs
+++ TBOBackEnd/Database/SeedData.cs
@@ -16,16 +16,17 @@ namespace TBOBackEnd.Database
       using (var scope = host.Services.CreateScope())
       {
         var serviceProvider = scope.ServiceProvider;
+        var logger = serviceProvider.GetRequiredService<ILogger<Program>>();
         try
         {
           var context = serviceProvider.GetRequiredService<_AppDbContext>();
           context.Database.EnsureCreated();
           SeedAdmins(context);
           SeedAdminRoles(context);
+          logger.LogInformation("Seeding the database has been successfully finished.");
         }
         catch (Exception e)
         {
-          var logger = serviceProvider.GetRequiredService<ILogger<Program>>();
           logger.LogError(e, "An error occurred while seeding the database.");
         }
       }
