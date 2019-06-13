8feb65af70c5ec06f0860831538380da96517e52
Pro Dev <prodev9999@gmail.com>
2019-06-14 01:21:27 +0800

    Added seed database mechanism

---
+++ TBOBackEnd/Database/SeedData.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TBOBackEnd.Models;
+
+namespace TBOBackEnd.Database
+{
+  public static class SeedData
+  {
+    public static void Initialize(IServiceProvider serviceProvider)
+    {
+      var context = serviceProvider.GetRequiredService<_AppDbContext>();
+      context.Database.EnsureCreated();
+      if (!context.Admins.Any())
+      {
+        context.Admins.AddRange(new List<Admin>
+        {
+
+        });
+        context.SaveChanges();
+      }
+    }
+  }
+}
--- TBOBackEnd/Program.cs
+++ TBOBackEnd/Program.cs
@@ -6,7 +6,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using TBOBackEnd.Database;
 
 namespace TBOBackEnd
 {
@@ -14,7 +16,23 @@ namespace TBOBackEnd
   {
     public static void Main(string[] args)
     {
-      CreateWebHostBuilder(args).Build().Run();
+      var host = CreateWebHostBuilder(args).Build();
+
+      using (var scope = host.Services.CreateScope())
+      {
+        var services = scope.ServiceProvider;
+        try
+        {
+          SeedData.Initialize(services);
+        }
+        catch (Exception e)
+        {
+          var logger = services.GetRequiredService<ILogger<Program>>();
+          logger.LogError(e, "An error occurred while seeding the database.");
+        }
+      }
+
+      host.Run();
     }
 
     public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
