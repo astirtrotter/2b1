0cc7c2fecbf74b7fb0204a3c3fd9bac4d1ebdd4f
Pro Dev <prodev9999@gmail.com>
2019-06-14 01:00:54 +0800

    Add database migration

---
+++ TBOBackEnd/Migrations/20190613164843_InitialCreate.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TBOBackEnd.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AdminPermissions",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    Name = table.Column<string>(maxLength: 50, nullable: false),
+                    Description = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AdminPermissions", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AdminRoles",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    Name = table.Column<string>(maxLength: 50, nullable: false),
+                    Description = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AdminRoles", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AdminRolePermission",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    AdminRoleId = table.Column<string>(nullable: false),
+                    AdminPermissionId = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AdminRolePermission", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AdminRolePermission_AdminPermissions_AdminPermissionId",
+                        column: x => x.AdminPermissionId,
+                        principalTable: "AdminPermissions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_AdminRolePermission_AdminRoles_AdminRoleId",
+                        column: x => x.AdminRoleId,
+                        principalTable: "AdminRoles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Admins",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    FirstName = table.Column<string>(maxLength: 50, nullable: false),
+                    LastName = table.Column<string>(maxLength: 50, nullable: false),
+                    Email = table.Column<string>(nullable: false),
+                    AdminRoleId = table.Column<string>(nullable: false),
+                    LastAccessedAt = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Admins", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Admins_AdminRoles_AdminRoleId",
+                        column: x => x.AdminRoleId,
+                        principalTable: "AdminRoles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AdminTokens",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    AdminId = table.Column<string>(nullable: false),
+                    LastAccessedAt = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AdminTokens", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AdminTokens_Admins_AdminId",
+                        column: x => x.AdminId,
+                        principalTable: "Admins",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AdminRolePermission_AdminPermissionId",
+                table: "AdminRolePermission",
+                column: "AdminPermissionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AdminRolePermission_AdminRoleId",
+                table: "AdminRolePermission",
+                column: "AdminRoleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Admins_AdminRoleId",
+                table: "Admins",
+                column: "AdminRoleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AdminTokens_AdminId",
+                table: "AdminTokens",
+                column: "AdminId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AdminRolePermission");
+
+            migrationBuilder.DropTable(
+                name: "AdminTokens");
+
+            migrationBuilder.DropTable(
+                name: "AdminPermissions");
+
+            migrationBuilder.DropTable(
+                name: "Admins");
+
+            migrationBuilder.DropTable(
+                name: "AdminRoles");
+        }
+    }
+}
