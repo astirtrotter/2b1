ce9e0bf51a2850d09546a510af76e7023ebb2b00
Pro Dev <prodev9999@gmail.com>
2019-06-15 17:37:26 +0800

    Added log4net

--- TBOBackEnd/Startup.cs
+++ TBOBackEnd/Startup.cs
@@ -70,8 +70,10 @@ namespace TBOBackEnd
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-    public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+    public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
     {
+      loggerFactory.AddLog4Net();
+
       app.UseCors("default");
 
       if (env.IsDevelopment())
