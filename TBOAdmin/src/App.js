79e763b10042623bb373c9a753fefda0bf187dee
Pro Dev <prodev9999@gmail.com>
2019-06-19 04:36:06 +0800

    Replace HashRouter with general Router

--- TBOAdmin/src/App.js
+++ TBOAdmin/src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
-import { HashRouter, Route, Switch } from 'react-router-dom';
+import { HashRouter, Router, Route, Switch } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
 // import { renderRoutes } from 'react-router-config';
 import './App.scss';
 
 const loading = () => <div className="animated fadeIn pt-3 text-center">Loading...</div>;
+const history = createBrowserHistory();
 
 // Containers
 const DefaultLayout = React.lazy(() => import('./containers/DefaultLayout'));
@@ -18,7 +20,7 @@ class App extends Component {
 
   render() {
     return (
-      <HashRouter>
+      <Router history={history}>
         <React.Suspense fallback={loading()}>
           <Switch>
             <Route exact path="/login" name="Login Page" render={props => <Login {...props}/>} />
@@ -28,7 +30,7 @@ class App extends Component {
             <Route path="/" name="Home" render={props => <DefaultLayout {...props}/>} />
           </Switch>
         </React.Suspense>
-      </HashRouter>
+      </Router>
     );
   }
 }
