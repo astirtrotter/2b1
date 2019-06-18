75597de87f41b4efb46266c1b2fcc24f6e724eac
Pro Dev <prodev9999@gmail.com>
2019-06-19 00:21:34 +0800

    Renamed package utils -> helpers

--- TBOBackEnd/Database/SeedData.Admins.cs
+++ TBOBackEnd/Database/SeedData.Admins.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using TBOBackEnd.Models;
-using TBOBackEnd.Utils;
+using TBOBackEnd.Helpers;
 
 namespace TBOBackEnd.Database
 {
--- TBOBackEnd/Database/SeedData.cs
+++ TBOBackEnd/Database/SeedData.cs
@@ -7,7 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using TBOBackEnd.Models;
-using TBOBackEnd.Utils;
+using TBOBackEnd.Helpers;
 
 namespace TBOBackEnd.Database
 {
